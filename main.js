e3aed38f5c7c43332674b7c30bf91277f0ca11d8
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-02-26 18:08:28 -0600

    finalizado

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Cantidad from "./cantidad.js";
 import Ingrediente from "./ingrediente.js";
+import Receta from "./receta.js";
 
 class Main {
   constructor() {
@@ -9,6 +10,7 @@ class Main {
       "de Sal",
       15
     );
+    this.receta = new Receta("Pizza Hawaiana", "Edgar Gonzalez", [new Ingrediente(new Cantidad(100, "gramos"), "de harina", 100), new Ingrediente(new Cantidad(5, "rebanadas"), "de piña", 230)])
   }
   pruebaCantidad() {
     console.log(this.cantidad.getDescripcion());
@@ -16,7 +18,12 @@ class Main {
   pruebaIngrediente() {
     console.log(this.ingrediente.getDescripcion());
   }
+  pruebaReceta() {
+      console.log(this.receta.getNumeroIngredientes())
+      console.log(this.receta.getCosto())
+      this.receta.imprimirEnConsola()
+  }
 }
 
 let app = new Main();
-app.pruebaIngrediente();
+app.pruebaReceta();
